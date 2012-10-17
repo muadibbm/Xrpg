189e3d38cb08b9ef1c883602bc964008345d43fe
Mehrdad <muadibbm@yahoo.com>
2012-10-17 01:24:10 -0400

    added empty classes, started game transfer to javascript

---
+++ jscripts/const.js
@@ -0,0 +1,7 @@
+//TODO : merge the funitonality of const script and content manager script
+// Used to download all needed resources from the webserver
+function ContentManager() {
+	//Method returns when all elements have been downloaded
+	var WINDOW_WIDTH = 0;
+	var WINDOW_HEIGHT = 0;
+}
