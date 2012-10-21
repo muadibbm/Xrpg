c9ee7d33e6cec023ce8b7ef1f1ceb38032a12180
apaunov <akpaunov@gmail.com>
2012-10-21 00:12:30 -0400

    Moved the loading of the images into a separate file

---
+++ jscripts/content.js
@@ -0,0 +1,6 @@
+﻿// Used to download all needed resources from the webserver
+var manifest = [
+    { id: "background", src: "images/Background/bg.png" }
+];
+
+
--- jscripts/initialize.js
+++
@@ -1,4 +0,0 @@
-﻿// Used to download all needed resources from the webserver
-function ContentManager() {
-	//Method returns when all elements have been downloaded
-}
