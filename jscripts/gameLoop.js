c5758a46b1f30b5c4d8e5a00dd25fc6829d70e2d
apaunov <akpaunov@gmail.com>
2012-11-06 23:30:29 -0500

    Commented out the .

--- jscripts/gameLoop.js
+++ jscripts/gameLoop.js
@@ -41,7 +41,7 @@ function prepareGame() {
 
     // Add key events listeners
     document.onkeydown = function (e) {
-        .handleKeyDown(e);
+        //.handleKeyDown(e);
     };
 
     startGame(); // Call the game loop to start
