ad04e4281db9f04f239023ee4139f7ebce774b6b
apaunov <akpaunov@gmail.com>
2012-11-06 23:00:44 -0500

    Keyboard stuff not complete

--- jscripts/keyboardEvents.js
+++ jscripts/keyboardEvents.js
@@ -1,1 +1,13 @@
-﻿
+﻿addKeyboardEvents = function (env) {
+    env.getBgBitmap().onPress = function () {
+        console.log("BG click!");
+    }
+
+    env.getTreeOfLifeBitmap().onPress = function () {
+        console.log("Tree click!");
+    }
+
+    env.getCaveBitmap().onPress = function () {
+        console.log("Cave click!");
+    }
+}
